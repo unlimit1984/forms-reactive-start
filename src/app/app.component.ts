e431fc844987c7af1e5c71934e92dc6b7b2a0699
Vladimir Vysokomornyi <unlimit_rmc@mail.ru>
2020-02-17 18:31:30 +0100

    ReactiveForms [214] setValue, patchValue, reset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,10 +27,26 @@ export class AppComponent implements OnInit {
     this.signupForm.statusChanges.subscribe(
       (value) => console.log(value)
     );
+    this.signupForm.setValue({
+        'userData': {
+          'username': 'Max',
+          'email': 'max@test.com'
+        },
+        'gender': 'male',
+        'hobbies': []
+      }
+    );
+    this.signupForm.patchValue({
+        'userData': {
+          'username': 'Anna'
+        }
+      }
+    );
   }
 
   onSubmit() {
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
   getControls() {

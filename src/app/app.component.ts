0ee8df05b8b2bb0d2e62b5e6d72e954eec9e1f42
Vladimir Vysokomornyi <unlimit_rmc@mail.ru>
2020-05-03 13:01:24 +0200

    ReactiveForms - Add remove hobby button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -65,6 +65,10 @@ export class AppComponent implements OnInit {
     (<FormArray>this.signupForm.get('hobbies')).push(control);
   }
 
+  onRemoveHobby(i: number) {
+    (<FormArray>this.signupForm.get('hobbies')).removeAt(i);
+  }
+
   forbiddenNames(control: FormControl): { [s: string]: boolean } {
     if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
       return {'nameIsForbidden': true};
@@ -106,5 +110,9 @@ export class AppComponent implements OnInit {
         'hobbies': ['PatchedValue']
       }, {emitEvent: false} // For disabling valueChanges/statusChanges event emitting
     );
+  }
+
+  onRemoveOneControl() {
+    this.signupForm.removeControl('gender');
   }
 }
